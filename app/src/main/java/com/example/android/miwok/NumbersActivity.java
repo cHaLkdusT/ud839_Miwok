7327dd23a859597fd7c644ce7913bbb2b6f18b58
Julius Lundang <jlundang@cambridge.org>
2016-08-21 23:14:01 +0800

    Array to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,6 +19,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -26,12 +28,27 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-        int index = 0;
-        for (String str : words) {
-            Log.v("NumbersActivity", "Word at index " + index + ": "+ str);
-            index++;
-        }
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        Log.v("NumbersActivity", "Word at index 0: " +  words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " +  words.get(1));
+        Log.v("NumbersActivity", "Word at index 2: " +  words.get(2));
+        Log.v("NumbersActivity", "Word at index 3: " +  words.get(3));
+        Log.v("NumbersActivity", "Word at index 4: " +  words.get(4));
+        Log.v("NumbersActivity", "Word at index 5: " +  words.get(5));
+        Log.v("NumbersActivity", "Word at index 6: " +  words.get(6));
+        Log.v("NumbersActivity", "Word at index 7: " +  words.get(7));
+        Log.v("NumbersActivity", "Word at index 8: " +  words.get(8));
+        Log.v("NumbersActivity", "Word at index 9: " +  words.get(9));
 
     }
 }
