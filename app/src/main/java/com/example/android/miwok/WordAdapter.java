f147f9924eba7b763c38ba8433b1dd697744c74f
Julius Lundang <jlundang@cambridge.org>
2016-08-27 20:19:04 +0800

    Different background colors for list item

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,11 +1,14 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,14 +17,16 @@ import java.util.ArrayList;
  * Created by jlundang on 25/08/2016.
  */
 public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResoureId;
     /**
      * Custom constructor. The context is used to inflate the layout file, and the list is the data
      * we want to populate into the lists.
      * @param context
      * @param words
      */
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        this.mColorResoureId = colorResourceId;
     }
 
     /**
@@ -63,6 +68,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             iconView.setVisibility(View.GONE);
         }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResoureId);
+        // Set the background color of the text container view
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
