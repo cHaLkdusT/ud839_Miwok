09a9eddbdc82da58a9a9e9020810e4d96294f407
Julius Lundang <jlundang@cambridge.org>
2016-08-28 19:01:48 +0800

    Challenge: Complete the Audio Feature

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,18 +10,22 @@ public class Word {
     private String mDefaultTranslation;
     /** Image resource ID */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
-
     private static final int NO_IMAGE_PROVIDED = -1;
+    /** Audio resource ID */
+    private int mAudioResourceId;
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
         this.mImageResourceId = imageResourceId;
+        this.mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
+        this.mAudioResourceId = audioResourceId;
     }
 
     /**
@@ -78,5 +82,21 @@ public class Word {
      */
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    /**
+     * Get the audio resource id for this word
+     * @return
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    /**
+     * We don't want this to be called outside this class
+     * @param mAudioResourceId
+     */
+    public void setAudioResourceId(int mAudioResourceId) {
+        this.mAudioResourceId = mAudioResourceId;
     }
 }
