51c86518789f8b6fb5fd2707bbbea8793cb8d7b2
Julius Lundang <jlundang@cambridge.org>
2016-08-29 23:09:20 +0800

    Add Tabs to ViewPager

--- app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,9 +9,12 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by jlundang on 29/08/2016.
  */
 public class CategoryFragmentPagerAdapter extends FragmentPagerAdapter {
+    /** Context of the app */
+    private Context mContext;
 
-    public CategoryFragmentPagerAdapter(FragmentManager fm) {
+    public CategoryFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        this.mContext = context;
     }
 
     @Override
@@ -30,5 +34,19 @@ public class CategoryFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else if (position == 3) {
+            return mContext.getString(R.string.category_phrases);
+        }
+        return null;
     }
 }
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -31,8 +32,12 @@ public class MainActivity extends AppCompatActivity {
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryFragmentPagerAdapter adapter = new CategoryFragmentPagerAdapter(getSupportFragmentManager());
+        CategoryFragmentPagerAdapter adapter = new CategoryFragmentPagerAdapter(this, getSupportFragmentManager());
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
